6a42828e0458c7361304aecb18cb95bd828a6a37
anas1306 <mohammed.anas1306@gmail.com>
2025-05-26 00:20:45 +0300

    added slider + fixed grid breaking out

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createGrid(sideLength) {
+function changeSides(sideLength = 16) {
 
     j = grid.children.length;
     while (j > 0) {
@@ -7,12 +7,12 @@ function createGrid(sideLength) {
     }
 
     i = sideLength;
-    heightWidth = (960/sideLength) - 4;
+    heightWidth = (512/sideLength) - 2;
     boxNumber = i * i;
     while (boxNumber > 0) {
         box = document.createElement('div');
         box.classList.add("interactable");
-        box.style.border = 'solid 2px black';
+        box.style.border = 'solid 1px black';
         box.style.backgroundColor = "white";
         box.style.height = `${heightWidth}px`;
         box.style.width = `${heightWidth}px`;
@@ -22,16 +22,16 @@ function createGrid(sideLength) {
 
 }
 
+const sideValue = document.querySelector("#setSide")
 const grid = document.querySelector(".grid")
-const setGrid = document.querySelector("#setGrid")
+const slider = document.querySelector(".slider")
 
-setGrid.addEventListener("click", () => {
-    let length = parseInt(prompt("What is the length of each side?"))
-    if (length > 100) {
-        alert("You can't have that many squares!")
-    } else {
-        createGrid(length)
-    }
+slider.addEventListener("change", () => {
+    changeSides(slider.value)
+    sideValue.textContent = `Set grid sides: ${slider.value}px`
+
 })
+
+changeSides()
 // okay so basically, first I'm gonna get the grid div. And then I'm gonna take the user's inputted value for the grid's size and then hand that to the create grid function. In the create grid function, I'm gonna make it so that the grid is exactly that many divs, as well as making them wrap so that they can only fit a certain amount of them.
 
